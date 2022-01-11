dbf232840cfad01fb84174e16843b49e0be81c22
Jonatan Escobar <escobarjonatan@aggienetwork.com>
2022-01-10 22:55:44 -0600

    Modifying the preview image

--- src/components/MetaHead/MetaHead.jsx
+++ src/components/MetaHead/MetaHead.jsx
@@ -15,16 +15,19 @@ const MetaHead = () => (
       content="Republic Barbershop in Dallas, TX"
     />
     {/* TODO: ONCE IMAGE IS UPLOADED WE CAN CHANGE THE HOST BELOW*/}
-    <meta property="og:image" content="../public/republic.jpeg" />
+    <meta property="og:image" content="https://placeimg.com/400/400/people" />
     <meta property="og:image:width" content="720" />
     <meta property="og:image:height" content="519" />
     <meta itemProp="name" content="Republic Barbershop" />
     <meta itemProp="url" content="https://therepublicbarbershop.com/" />
     <meta itemProp="description" content="Republic Barbershop in Dallas, TX" />
-    <meta itemProp="thumbnailUrl" content="../public/republic.jpeg" />
-    <meta itemProp="image" content="../public/republic.jpeg" />
+    <meta
+      itemProp="thumbnailUrl"
+      content="https://placeimg.com/400/400/people"
+    />
+    <meta itemProp="image" content="https://placeimg.com/400/400/people" />
     <meta name="twitter:title" content="Republic Barbershop" />
-    <meta name="twitter:image" content="../public/republic.jpeg" />
+    <meta name="twitter:image" content="https://placeimg.com/400/400/people" />
     <meta name="twitter:url" content="https://therepublicbarbershop.com/" />
     <meta name="twitter:card" content="summary" />
     <meta
